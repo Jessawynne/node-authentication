120048062bc9ee190594680dabcb9d530206e069
Jessawynne <jessawynne@gmail.com>
2016-02-22 11:16:58 -0600

    counting visits via redisStore

--- server.js
+++ server.js
@@ -20,8 +20,10 @@ app.use(session({
 
 //middleware function for session obj
 app.use((req, res, next) => {
-  req.session.count = req.session.count || 0;
-  req.session.count++;
+  req.session.visits = req.session.visits || {};
+
+  req.session.visits[req.url] = req.session.visits[req.url] || 0;
+  req.session.visits[req.url]++;
   console.log(req.session);
   next();
 });
