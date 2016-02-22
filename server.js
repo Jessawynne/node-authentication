9e871b531910b5dad2c777b2db985c390ded9411
Jessawynne <jessawynne@gmail.com>
2016-02-22 10:25:36 -0600

    boilerplate for session token

--- server.js
+++ server.js
@@ -2,13 +2,18 @@
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const session = require('express-session');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+const SESSION_SECRET = process.env.SESSION_SECRET || 'supersecret';
 
 app.set('view engine', 'jade');
 
 app.use(bodyParser.urlencoded({extended: false}));
+app.use(session({
+  secret: SESSION_SECRET
+}));
 
 app.get('/', (req, res) => {
   res.render('index');
